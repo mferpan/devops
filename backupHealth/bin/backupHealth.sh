33a7cc0656f0303b459fb0e27ab5a5c62dbb4896
manuel fernandez panzuela <manuel@mfernandez.es>
2016-12-20 22:17:52 +0100

    new version

--- backupHealth/bin/backupHealth.sh
+++ backupHealth/bin/backupHealth.sh
@@ -8,57 +8,66 @@ ARCHIVED="${BASE}/archived"
 VZ_FOLDER="${CLIENTS_BASE}/vzdump"
 LOGS_DIR="${BASE}/logs"
 INBOX="${BASE}/inbox"
-
 FM_CMD="/usr/bin/fetchmail"
+
 DATE=$(date +%Y%m%d)
+LOG="${LOGS_DIR}/fetchmailrc-${DATE}.log"
 COMPRESS_DAY="1"
 
-REPORT_MAIL_ADDRESS="manuel@mfernandez.es"
-SENDER="backup-verifier@grupoinova.es"
+REPORT_MAIL_ADDRESS="glpi@inovages.es"
+SENDER="backup@grupoinova.es"
 
 TOTAL_CLIENTS=$(ls ${CLIENTS_BASE} | wc -l)
 
 # To Do
-# Verify that each company has backup emails
+# Verify that for each company we have got backup emails
 
 # -------- Functions --------
 downloadEmail() {
-	local LOG="${LOGS_DIR}/fetchmailrc-${DATE}.log"
-
-	[ ! -d ${INBOX} ] && mkdir -p ${INBOX}
-	[ ! -f ${LOG} ] && touch ${LOG}
-	${FM_CMD} --fetchmailrc ${CONF_DIR}/fetchmailrc --logfile ${LOG}
+        [ ! -d ${INBOX} ] && mkdir -p ${INBOX}
+        [ ! -f ${LOG} ] && touch ${LOG}
+        echo "# -------------- Mail Download Process starting - $(date +%Y/%m/%d-%H:%M:%S) -------------- #" >> ${LOG}
+        ${FM_CMD} --fetchmailrc ${CONF_DIR}/fetchmailrc --logfile ${LOG}
 }
 
 sendReport() {
-	local OK=$1
-	local WR=$2
-	local ER=$3
-	local BODY="Backup report generated at ${DATE} <br /><br /><br />"
+    local OK=($(echo "$1" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+    local WR=($(echo "$2" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+    local ER=($(echo "$3" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+    local BODY="Backup report generated at $(date +%Y/%m/%d-%H:%M:%S) <br /><br /><br />"
+
+	if [ ! -z "$OK" ]; then
+		SUBJECT="[OK] There are ${#OK[@]} backups correct"
+		BODY+="There are ${#OK[@]} clients with backups in <font color=\"green\">OK</font> state: <br /><font color=\"green\">"
+		for host in ${OK[*]}; do BODY+=" > ${host}<br />"; done
+		BODY+="</font><br />"
+	fi
+
+	if [ ! -z "$WR" ]; then
+		SUBJECT="[WARNING] There are ${#WR[@]} clients with warning"
+		BODY+="There are ${#WR[@]} clients with backups in <font color=\"orange\">WARNING</font> state: <br /><font color=\"orange\">"
+		for host in ${WR[*]}; do BODY+=" > ${host}<br />"; done
+		BODY+="</font><br />"
+	fi
 
 	if [ ! -z "$ER" ]; then
 		SUBJECT="[ERROR] There are ${#ER[@]} clients with errors"
-		BODY+="There are ${#ER[@]} clients with backups in ERROR state: <font color=\"red\"> ${ER[*]} </font><br />"
-	fi
-	if [ ! -z "$WR" ]; then
-		[ -z "$ER" ] && SUBJECT="[WARNING] There are ${#WR[@]} clients with warning"
-		BODY+="There are ${#WR[@]} clients with backups in WARNING state: <font color=\"orange\"> ${WR[*]} </font><br />"
-	fi
-
-	if [ ! -z "$OK" ] && [ -z "$WR" ] && [ -z "$ER" ]; then
-		SUBJECT="[OK] There are ${#OK[@]} backups correct"
-		BODY="There are ${#OK[@]} clients with backups in OK state: <font color=\"green\"> ${OK[*]} </font><br />"
-	else
-		BODY+="Clients with backups in OK state are: <font color=\"green\"> ${OK[*]} </font> <br />"
+		BODY+="There are ${#ER[@]} clients with backups in <font color=\"red\">ERROR</font> state: <br /><font color=\"red\">"
+		for host in ${ER[*]}; do BODY+=" > ${host}<br />"; done
+		BODY+="</font><br />"
 	fi
 
 	if [ -z "$OK" ] && [ -z "$WR" ] && [ -z "$ER" ]; then
 		SUBJECT="[ERROR] NO backups to check, Is it correct?"
-		BODY="<font color=\"red\">NO backups to check Is it correct?</font><br />"
+		BODY+="<font color=\"red\">NO backups to check Is it correct?</font><br />"
 	fi
+
+	echo "${BODY}" >> ${LOG}
+	echo -e "\n\n" >> ${LOG}
 
 	echo "${BODY}" | mutt -e "set realname=\"${SENDER}\"" -e "set content_type=text/html" -s "${SUBJECT}" "${REPORT_MAIL_ADDRESS}"
 }
+
 
 parseEmail() {
 	local CLIENTS=$(ls ${CLIENTS_BASE})
@@ -66,38 +75,50 @@ parseEmail() {
 	local CLIENTS_WR=""
 	local CLIENTS_ER=""
 
+ 	[ ! -d $ARCHIVED ] && mkdir -p ${ARCHIVED}
+
 	TOTAL_EMAILS=$(ls ${INBOX} | wc -l)
 
 	for CLI in $CLIENTS; do
 		for MAIL in $(ls ${INBOX}); do
-
 			# Checking Proxmox backups
-			if (grep "vzdump.*successful" ${INBOX}/${MAIL}); then
-				mv ${INBOX}/${MAIL} ${VZ_FOLDER}
-			elif (grep "vzdump.*error" ${INBOX}/${MAIL}); then
-				mv ${INBOX}/${MAIL} ${VZ_FOLDER}
-			elif (grep "vzdump.*warning" ${INBOX}/${MAIL}); then
-				mv ${INBOX}/${MAIL} ${VZ_FOLDER}
+			if (grep "^Subject.*${CLI}.*successful" ${INBOX}/${MAIL}); then
+				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
+				CLIENTS_OK+=" ${CLI}"
+			elif (grep "^Subject.*${CLI}.*failed" ${INBOX}/${MAIL}); then
+				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
+				CLIENTS_ER+=" ${CLI}"
 			fi
 
 			# Checking other emails
-			if (grep "Subject.*correctamente.*${CLI}" ${INBOX}/${MAIL}); then
+			if (grep "^Subject.*correctamente.*${CLI}" ${INBOX}/${MAIL}); then
 				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
 				CLIENTS_OK+=" ${CLI}"
-			elif (grep "Subject.*con.*errores.*${CLI}" ${INBOX}/${MAIL}); then
+			elif (grep "^Subject.*Correcto.*${CLI}" ${INBOX}/${MAIL}); then
 				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
-				CLIENTS_ER+=" ${CLI}"
-			elif  (grep "Subject.*con.*avisos.*${CLI}" ${INBOX}/${MAIL}); then
+				CLIENTS_OK+=" ${CLI}"
+			elif (grep "^Subject.*satisfactorio.*${CLI}" ${INBOX}/${MAIL}); then
+				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
+				CLIENTS_OK+=" ${CLI}"
+			elif  (grep "^Subject.*Aviso.*${CLI}" ${INBOX}/${MAIL}); then
 				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
 				CLIENTS_WR+=" ${CLI}"
-			else
-				# Spam mails will be deleted or archived
-				echo "[ARVCHIVING] Sending ${MAIL} to ${ARCHIVED} directory"
-				# rm -f ${INBOX}/${MAIL}
-				mv ${INBOX}/${MAIL} ${ARCHIVED}
+			elif  (grep "^Subject.*perdida.*${CLI}" ${INBOX}/${MAIL}); then
+				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
+				CLIENTS_WR+=" ${CLI}"
+			elif (grep "^Subject.*con.*errores.*${CLI}" ${INBOX}/${MAIL}); then
+				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
+				CLIENTS_ER+=" ${CLI}"
+			elif  (grep "^Subject.*Fallo.*${CLI}" ${INBOX}/${MAIL}); then
+				mv ${INBOX}/${MAIL} ${CLIENTS_BASE}/${CLI}
+				CLIENTS_ER+=" ${CLI}"
 			fi
 		done
 	done
+
+	# Spam mails will be archived
+	echo "[ARVCHIVING] Sending NON processed emails to ${ARCHIVED} directory"
+	mv ${INBOX}/*.eml ${ARCHIVED}
 
 	sendReport "${CLIENTS_OK}" "${CLIENTS_WR}" "${CLIENTS_ER}"
 }
@@ -127,10 +148,9 @@ cleanArchived() {
 }
 
 listClients() {
-	local CLIENT
 	echo "There are ${TOTAL_CLIENTS[*]} clients"
-	for CLIENT in $(ls ${CLIENTS_BASE}); do
-		echo "   ---> ${CLIENT}"
+	for C in $(ls ${CLIENTS_BASE}); do
+		echo "   ---> ${C}"
 	done
 }
 
@@ -145,7 +165,14 @@ createClient() {
 	[ $? -eq 0 ] && echo "[INFO] Client ${NAME} created sucessfully"
 }
 
+prepareInbox() {
+	mv ${ARCHIVED}/* ${INBOX}/
+}
+
 download() {
+	echo "[START] Prepare previous mails to be processed"
+	prepareInbox
+
 	echo "[START] Downloading emails process has started"
 	downloadEmail
 	parseEmail
@@ -169,7 +196,7 @@ menu() {
 			;;
 			"Quit")	break ;;
 	        *)
-				echo "[WARNING] Unknown option"
+			echo "[WARNING] Unknown option"
 	        ;;
 	    esac
 	done
